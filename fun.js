3b2fef65c3991c3ea831d1cbb17f963f1b5b9e8e
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-07-15 19:41:05 +0600

    lets do some js

---
+++ fun.js
@@ -0,0 +1,9 @@
+let myName = "Sabbir Ahmed Samir";
+// name = "Samir Ahmed";
+// name = "Samir";
+
+const fav = "Leo Messi";
+// fav = "Olise";
+
+console.log("My name is :", myName);
+console.log(typeof 7);
