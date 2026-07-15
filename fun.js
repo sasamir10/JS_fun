927d1537f3ea01d83ba9b0034850bc7fdb3993c3
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-07-15 22:06:14 +0600

    fun js

--- fun.js
+++ fun.js
@@ -1,9 +1,33 @@
-let myName = "Sabbir Ahmed Samir";
-// name = "Samir Ahmed";
-// name = "Samir";
+// practise
+/* let myName = "Sabbir Ahmed Samir";
 
 const fav = "Leo Messi";
-// fav = "Olise";
 
 console.log("My name is :", myName);
-console.log(typeof 7);
+console.log(typeof 7); */
+
+// Task-1
+/* console.log("Hello JavaScript!");
+console.log("My name is Samir");
+console.log("I am excited to become a Software Developer."); */
+
+// Task-2
+/* let num1 = 25;
+let num2 = 10;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2); */
+
+// Task-3
+let studentName = "Sabbir Ahmed";
+let age = 25;
+let country = "Bangkadesh";
+let isStudent = true;
+
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Country:", country);
+console.log("Is Student:", isStudent);
